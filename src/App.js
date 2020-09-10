882f96179141ec6a36480c972f19974cfb3f9eb2
anarubado <rubadoana@gmail.com>
2020-09-10 01:46:31 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [estaSonando, setEstaSonando] = useState(null);
+
+  const notes = [
+    {
+      name: "Do",
+      link: require("./notes/do.wav"),
+      sharp: true
+    },
+    {
+      name: "Re",
+      link: require("./notes/re.wav"),
+      sharp: true
+    },
+    {
+      name: "Mi",
+      link: require("./notes/mi.wav")
+    },
+    {
+      name: "Fa",
+      link: require("./notes/fa.wav"),
+      sharp: true
+    },
+    {
+      name: "Sol",
+      link: require("./notes/sol.wav"),
+      sharp: true
+    },
+    {
+      name: "La",
+      link: require("./notes/la.wav"),
+      sharp: true
+    },
+    {
+      name: "Si",
+      link: require("./notes/si.wav")
+    }
+  ];
+
+  const handleClick = (note) => {
+    const audio = new Audio(note.link);
+    audio.play();
+
+    setTimeout(() => {
+      setEstaSonando("");
+    }, 150);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {notes.map((note) => {
+          return (
+            <div
+              className={`nota ${estaSonando === note.name ? "estaSonando" : ""}`}
+              onClick={() => handleClick(note)}
+            >
+              {note.sharp ? <div className="nota-negra"></div> : ""}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
