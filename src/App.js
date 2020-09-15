14cd84d0d9dc5da9ed1a3432589414a3ca3fd7e1
anarubado <rubadoana@gmail.com>
2020-09-15 17:26:21 -0300

    Changed history
    Notes' files changed, layout changed so divs can react to events
    independently.

--- src/App.js
+++ src/App.js
@@ -2,66 +2,104 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [estaSonando, setEstaSonando] = useState(null);
+
+  const [isSounding, setIsSounding] = useState(null);
 
   const notes = [
     {
       name: "Do",
-      link: require("./notes/do.wav"),
-      sharp: true
+      link: require("./notes/C.mp3"),
+      key: "white"
     },
     {
+      name: "Do# / Reb",
+      link: require("./notes/CSharp.mp3"),
+      key: "black"
+    },      
+    {
       name: "Re",
-      link: require("./notes/re.wav"),
-      sharp: true
+      link: require("./notes/D.mp3"),
+      key: "white"
+    },
+    {
+      name: "Re# / Mib",
+      link: require("./notes/DSharp.mp3"),
+      key: "black"
     },
     {
       name: "Mi",
-      link: require("./notes/mi.wav")
+      link: require("./notes/E.mp3"),
+      key: "white"
     },
     {
       name: "Fa",
-      link: require("./notes/fa.wav"),
-      sharp: true
+      link: require("./notes/F.mp3"),
+      key: "white"
+    },
+    {
+      name: "Fa# / Solb",
+      link: require("./notes/FSharp.mp3"),
+      key: "black"
     },
     {
       name: "Sol",
-      link: require("./notes/sol.wav"),
-      sharp: true
+      link: require("./notes/G.mp3"),
+      key: "white"
+    },
+    {
+      name: "Sol# / Lab",
+      link: require("./notes/GSharp.mp3"),
+      key: "black"
     },
     {
       name: "La",
-      link: require("./notes/la.wav"),
-      sharp: true
+      link: require("./notes/A.mp3"),
+      key: "white"
+    },
+    {
+      name: "La# / Sib",
+      link: require("./notes/ASharp.mp3"),
+      key: "black"
     },
     {
       name: "Si",
-      link: require("./notes/si.wav")
+      link: require("./notes/B.mp3"),
+      key: "white"
     }
   ];
 
   const handleClick = (note) => {
+    setIsSounding(note.name);
     const audio = new Audio(note.link);
     audio.play();
-
     setTimeout(() => {
-      setEstaSonando("");
-    }, 150);
+      setIsSounding(null);
+    }, 300);
   };
+
   return (
     <div className="App">
-      <div className="container">
-        {notes.map((note) => {
+
+      <div className="title">
+        <h1>Piano con React</h1>
+        <h2>Hecho con Ada</h2>
+        {
+          isSounding && <h3>Está sonando la nota { isSounding }</h3>
+        }        
+      </div>
+
+      <span className="container">
+
+        {notes.map((note, idx) => {
           return (
-            <div
-              className={`nota ${estaSonando === note.name ? "estaSonando" : ""}`}
-              onClick={() => handleClick(note)}
-            >
-              {note.sharp ? <div className="nota-negra"></div> : ""}
+            <div className={`${note.key === "white" ? "white" : "black"} 
+            ${isSounding === note.name ? "isSounding" : ""}`} 
+            onClick={() => handleClick(note)} key={idx}>
             </div>
           );
         })}
-      </div>
+
+      </span>
     </div>
   );
 }
