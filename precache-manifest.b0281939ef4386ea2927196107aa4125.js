041263a3b19b26c2f4a6881abdf17d7b75a0f543
anarubado <rubadoana@gmail.com>
2020-09-19 19:54:18 -0300

    Updates

---
+++ precache-manifest.b0281939ef4386ea2927196107aa4125.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d15af13f2fcba73e92108ac5071b17",
+    "url": "/pianoWithReact/index.html"
+  },
+  {
+    "revision": "8127247eda119f34d241",
+    "url": "/pianoWithReact/static/css/main.a899798a.chunk.css"
+  },
+  {
+    "revision": "7d53f459b258d626c651",
+    "url": "/pianoWithReact/static/js/2.f246b197.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pianoWithReact/static/js/2.f246b197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8127247eda119f34d241",
+    "url": "/pianoWithReact/static/js/main.3e686ad2.chunk.js"
+  },
+  {
+    "revision": "a197a6b9649c2f531a81",
+    "url": "/pianoWithReact/static/js/runtime-main.4b2f557a.js"
+  },
+  {
+    "revision": "da1e3533b68c29739bf6bdb247176b20",
+    "url": "/pianoWithReact/static/media/a-4.da1e3533.mp3"
+  },
+  {
+    "revision": "0803afc48a032e1d7bebaa47423feec7",
+    "url": "/pianoWithReact/static/media/a-5.0803afc4.mp3"
+  },
+  {
+    "revision": "d4190dc79e549a985b1196112b11ddd4",
+    "url": "/pianoWithReact/static/media/a4.d4190dc7.mp3"
+  },
+  {
+    "revision": "b06ca26f3f670de2ee2d4aed9b3885f1",
+    "url": "/pianoWithReact/static/media/a5.b06ca26f.mp3"
+  },
+  {
+    "revision": "d3d070d435123195d01b342aa70c2719",
+    "url": "/pianoWithReact/static/media/b4.d3d070d4.mp3"
+  },
+  {
+    "revision": "7fa41f17fbffc17716e35fd1d5037746",
+    "url": "/pianoWithReact/static/media/b5.7fa41f17.mp3"
+  },
+  {
+    "revision": "2618fea95e670754e73b0439b34e02a9",
+    "url": "/pianoWithReact/static/media/bg.2618fea9.jpg"
+  },
+  {
+    "revision": "244ecaacf5b04c7bf13da79c264231d0",
+    "url": "/pianoWithReact/static/media/c-3.244ecaac.mp3"
+  },
+  {
+    "revision": "dec2ea2401a4602af74caac1765d03aa",
+    "url": "/pianoWithReact/static/media/c-4.dec2ea24.mp3"
+  },
+  {
+    "revision": "c9a928d5c072bf8adcd66e8292fa380d",
+    "url": "/pianoWithReact/static/media/c-5.c9a928d5.mp3"
+  },
+  {
+    "revision": "49c005eb36ea2e7a029a63b330a2c79c",
+    "url": "/pianoWithReact/static/media/c3.49c005eb.mp3"
+  },
+  {
+    "revision": "8bb14fcbcbcc488e400d0fca8460d242",
+    "url": "/pianoWithReact/static/media/c4.8bb14fcb.mp3"
+  },
+  {
+    "revision": "13dfe4a648312215f55d468f56a47f32",
+    "url": "/pianoWithReact/static/media/c5.13dfe4a6.mp3"
+  },
+  {
+    "revision": "707e411ab177101e65692eb0bf17cda7",
+    "url": "/pianoWithReact/static/media/d-3.707e411a.mp3"
+  },
+  {
+    "revision": "dbae0e5d1a99f11f9d5dbfeb60f0abc4",
+    "url": "/pianoWithReact/static/media/d-4.dbae0e5d.mp3"
+  },
+  {
+    "revision": "9ba007e131889abfecf076086b3a4e4b",
+    "url": "/pianoWithReact/static/media/d-5.9ba007e1.mp3"
+  },
+  {
+    "revision": "54ec56a1bea4695dfd3fc183c76ce34b",
+    "url": "/pianoWithReact/static/media/d3.54ec56a1.mp3"
+  },
+  {
+    "revision": "57dcd2c0aaf63dba8d8653eaf048a1e6",
+    "url": "/pianoWithReact/static/media/d4.57dcd2c0.mp3"
+  },
+  {
+    "revision": "ee27cadbc5f2019518ef89a3eaf02ba6",
+    "url": "/pianoWithReact/static/media/d5.ee27cadb.mp3"
+  },
+  {
+    "revision": "e40d513988d7e37cf134103f36a1b7af",
+    "url": "/pianoWithReact/static/media/e3.e40d5139.mp3"
+  },
+  {
+    "revision": "4697964efe65fd09a08c443a97ec208a",
+    "url": "/pianoWithReact/static/media/e4.4697964e.mp3"
+  },
+  {
+    "revision": "aa16d0937fe5f1c182dbe340cdc1d80c",
+    "url": "/pianoWithReact/static/media/e5.aa16d093.mp3"
+  },
+  {
+    "revision": "01d2c4f51bc14fcf4efcfa68ed189759",
+    "url": "/pianoWithReact/static/media/f-3.01d2c4f5.mp3"
+  },
+  {
+    "revision": "873c08ec94cd3a2300f5749718d39b2d",
+    "url": "/pianoWithReact/static/media/f-4.873c08ec.mp3"
+  },
+  {
+    "revision": "ec5e8804060d452969613239152d18a4",
+    "url": "/pianoWithReact/static/media/f3.ec5e8804.mp3"
+  },
+  {
+    "revision": "08e83d93c6b074995bd23a1e671e8192",
+    "url": "/pianoWithReact/static/media/f4.08e83d93.mp3"
+  },
+  {
+    "revision": "cbe82aee01255000fed53c51e0c193fc",
+    "url": "/pianoWithReact/static/media/g-3.cbe82aee.mp3"
+  },
+  {
+    "revision": "add7532288ba881c8b2a9c72e504e62b",
+    "url": "/pianoWithReact/static/media/g-4.add75322.mp3"
+  },
+  {
+    "revision": "eee0643b490be7e09f75d39cea2e5858",
+    "url": "/pianoWithReact/static/media/g3.eee0643b.mp3"
+  },
+  {
+    "revision": "aca1ebb44308bf70376fe1dd957ef818",
+    "url": "/pianoWithReact/static/media/g4.aca1ebb4.mp3"
+  }
+]);
